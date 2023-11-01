81ff83930e00385cc98c8f2b953866c98e8a82ba
Benny Schulz <schulz33@msu.edu>
2023-11-01 17:27:50 -0400

    project

---
+++ Synthie/OrganFactory.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <vector>
+#include "Organ.h"
+
+class COrganFactory
+{
+public:
+	COrganFactory();
+	virtual ~COrganFactory();
+
+	COrgan *COrganFactory::CreateOrgan(double bpm);
+	void COrganFactory::SetNote(CNote *note);
+	void SetFactoryDrawbars(int drawbarNum);
+
+private:
+	double m_amp;
+
+	double m_vibratoFreq;
+	double m_vibratoMag;
+
+	bool m_leslieChanged;
+	double m_leslieFreq;
+	double m_leslieStartFreq;
+	double m_leslieEndFreq;
+	double m_leslieRadius;
+	double m_leslieAttackTime;
+	double m_leslieReleaseTime;
+
+	int m_drawbarNum;
+	vector<double> m_drawbars;
+
+	vector<int> m_harmonics;
+};
+
